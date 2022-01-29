b0798357b5603142314c5e4e7dd1026ca310c94a
Olga Santarovich <santarovich@gmail.com>
2022-01-29 23:19:37 +0300

    easy/if_elif_else/payment_card fixed

--- tasks/easy/if_elif_else/payment_card.py
+++ tasks/easy/if_elif_else/payment_card.py
@@ -22,8 +22,10 @@ def hide_card_numbers(card_number: str) -> str:
     :return: строка с номером карты со звездочками или строка "Ошибка"
     :rtype: str
     """
-    # TODO написать свой код здесь
-    return None
+    if card_number.isdigit():
+        return f"{card_number[:4]}{'*' * 8}{card_number[-4:]}"
+    else:
+        return "Ошибка"
 
 
 if __name__ == '__main__':
