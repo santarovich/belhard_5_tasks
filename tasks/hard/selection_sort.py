8a579e4958ab4a03784baf52b5e0610a9d6023f9
Olga Santarovich <santarovich@gmail.com>
2022-02-07 12:26:17 +0300

    hard/selection_sort fixed

--- tasks/hard/selection_sort.py
+++ tasks/hard/selection_sort.py
@@ -19,6 +19,12 @@
 
 
 def selection_sort(array: list) -> list:
+    for i in range(len(array) - 1):
+        min_index = i
+        for j in range(i + 1, len(array)):
+            if array[min_index] > array[j]:
+                min_index = j
+        array[min_index], array[i] = array[i], array[min_index]
     return array
 
 
