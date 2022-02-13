fb30ad887b65648a872ff0106091b1ccc8cee31d
Olga Santarovich <santarovich@gmail.com>
2022-02-13 21:22:19 +0300

    hard/insertion_sort done

--- tasks/hard/insertion_sort.py
+++ tasks/hard/insertion_sort.py
@@ -21,7 +21,13 @@
 
 
 def insertion_sort(array: list) -> list:
-    pass
+    for i in range(len(array) - 1):
+        min_index = i
+        for j in range(i + 1, len(array)):
+            if array[min_index] > array[j]:
+                min_index = j
+        array[min_index], array[i] = array[i], array[min_index]
+    return array
 
 
 if __name__ == '__main__':
